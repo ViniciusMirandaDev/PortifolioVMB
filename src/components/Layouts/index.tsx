80edf9863cfa81b8602e41ac1c09852842e3da37
Vinicius Miranda <vinimb07@gmail.com>
2022-09-29 12:13:44 -0300

    fix: smooth scrooling again and build

--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -1,13 +1,16 @@
+import SmoothScroll from '@/lib/react-smooth-scrolling'
+
 import Footer from '../Footer'
 import Header from '../Header'
 import { Container } from './styles'
-
 export const BasicLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Container>
-      <Header />
-      <main>{children}</main>
-      <Footer />
+      <SmoothScroll skew>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </SmoothScroll>
     </Container>
   )
 }
